c982af013659cacd35cd1d5e065449a1a333fca8
yghjava <=>
2017-07-12 20:17:59 +0800

    commit_question1009

---
+++ question1009/code.c
@@ -0,0 +1,188 @@
+/*
+ * code.c
+ *
+ *  Created on: 2017年6月5日
+ *      Author: ygh
+ *  Algorithms thoughts:
+ *  We will use tow linked lists for polynomial A
+ *  and B,then traversal them to get sum
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node *ptrPoly;
+typedef struct node {
+	/**
+	 * The exponents for the polynomial
+	 */
+	int exponents;
+	/**
+	 * The coefficient for polynomial
+	 */
+	double coefficient;
+
+	/**
+	 * A point to point next polynomial term
+	 */
+	struct node *next;
+};
+
+ptrPoly buildPoly(int size) {
+	ptrPoly head = (ptrPoly) malloc(sizeof(struct node));
+	ptrPoly rear = head;
+	int exponents;
+	double coefficient;
+	int i;
+	head->next = NULL;
+	for (i = 0; i < size; i++) {
+		scanf("%d %lf", &exponents, &coefficient);
+		ptrPoly p = (ptrPoly) malloc(sizeof(struct node));
+		p->exponents = exponents;
+		p->coefficient = coefficient;
+		p->next = rear->next;
+		rear->next = p;
+		rear = p;
+	}
+	return head;
+}
+
+void release(ptrPoly head) {
+	ptrPoly p = head->next;
+	ptrPoly temp;
+	while (p) {
+		temp = p;
+		p = p->next;
+		free(temp);
+	}
+	free(head);
+}
+
+void add(ptrPoly *rear, ptrPoly p) {
+	ptrPoly temp = (ptrPoly) malloc(sizeof(struct node));
+	temp->exponents = p->exponents;
+	temp->coefficient = p->coefficient;
+	temp->next = (*rear)->next;
+	(*rear)->next = temp;
+	(*rear) = temp;
+}
+
+/**
+ * A method implement two polynomials add
+ */
+ptrPoly getSum(ptrPoly a, ptrPoly b) {
+	ptrPoly head = (ptrPoly) malloc(sizeof(struct node));
+	ptrPoly rear = head;
+	ptrPoly p = a->next;
+	ptrPoly q = b->next;
+	while (p && q) {
+		if (p->exponents > q->exponents) {
+			add(&rear, p);
+			p = p->next;
+		} else if (p->exponents < q->exponents) {
+			add(&rear, q);
+			q = q->next;
+		} else {
+			/**
+			 * If the coefficient add is equal 0,we will not to
+			 * add it
+			 */
+			if (p->coefficient + q->coefficient != 0) {
+				ptrPoly temp = (ptrPoly) malloc(sizeof(struct node));
+				temp->exponents = p->exponents;
+				temp->coefficient = p->coefficient + q->coefficient;
+				temp->next = NULL;
+				add(&rear, temp);
+				free(temp);
+			}
+			p = p->next;
+			q = q->next;
+		}
+	}
+	while (p) {
+		add(&rear, p);
+		p = p->next;
+	}
+	while (q) {
+		add(&rear, q);
+		q = q->next;
+	}
+	release(a);
+	release(b);
+	return head;
+}
+
+/**
+ * A method implement two polynomials multiple,but we use add
+ * algorithms
+ */
+ptrPoly multiply(ptrPoly a, ptrPoly b) {
+	ptrPoly head = (ptrPoly) malloc(sizeof(struct node));
+	ptrPoly p = a->next;
+
+	while (p) {
+		ptrPoly q = b->next;
+		ptrPoly temp = (ptrPoly) malloc(sizeof(struct node));
+		ptrPoly rear = temp;
+		temp->next=NULL;
+		while (q) {
+			/**
+			 * If the coefficient add is equal 0,we will not to
+			 * add it
+			 */
+			if (p->coefficient * q->coefficient != 0) {
+				ptrPoly newNode = (ptrPoly) malloc(sizeof(struct node));
+				newNode->exponents = p->exponents + q->exponents;
+				newNode->coefficient = p->coefficient * q->coefficient;
+				newNode->next = NULL;
+				add(&rear, newNode);
+				free(newNode);
+			}
+			q = q->next;
+		}
+		head = getSum(head, temp);
+		p = p->next;
+	}
+
+	return head;
+}
+
+void toString(ptrPoly head) {
+	ptrPoly p = head->next;
+	int cnt = 0, i = 0;
+	while (p) {
+		cnt++;
+		p = p->next;
+	}
+	if (cnt == 0) {
+		printf("%d", cnt);
+	} else {
+		printf("%d ", cnt);
+	}
+	p = head->next;
+	while (p) {
+		i++;
+		if (i == cnt) {
+			printf("%d %.1lf", p->exponents, p->coefficient);
+		} else {
+			printf("%d %.1lf ", p->exponents, p->coefficient);
+		}
+
+		p = p->next;
+	}
+	printf("\n");
+}
+
+int main() {
+	int aSize, bSize;
+	ptrPoly aPloy, bPloy, sum;
+	scanf("%d", &aSize);
+	aPloy = buildPoly(aSize);
+	scanf("%d", &bSize);
+	bPloy = buildPoly(bSize);
+	sum = multiply(aPloy,bPloy);
+	toString(sum);
+	release(aPloy);
+	release(bPloy);
+	release(sum);
+	return 0;
+}
