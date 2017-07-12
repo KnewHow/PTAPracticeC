ae5afeba9e6030074a68e90d9059163e65a6d9fd
yghjava <=>
2017-07-12 20:17:59 +0800

    commit_justtest

---
+++ justtest/test.c
@@ -0,0 +1,13 @@
+/*
+ * test.c
+ *
+ *  Created on: 2017年6月3日
+ *      Author: ygh
+ *  A test project at the first time
+ */
+#include <stdio.h>
+int main(){
+	printf("just test");
+	return 0;
+}
+
